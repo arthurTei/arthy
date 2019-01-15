e187c88f97cd341141faca113f5b5a279b5844a7
arthurTei <46706285+arthurTei@users.noreply.github.com>
2019-01-15 12:59:20 +0100

    my first commit

---
+++ lesson1/03.variables/script.js
@@ -0,0 +1,35 @@
+// 'user strict';
+
+// let user; // declare user variable//
+// let admin; // declare admine variable//
+
+// user = 'Sophie'; // assigner une valeur à une variable//
+// admin = user;        // copier la valeur d'user dans admin
+// console.log(admin); // loggez la valeur d'admin dans la console
+
+// user = 'Mathieu' // reassignez la valeur d'une variable //
+// console.log(user); // loggez la valeur d'admin dans la console
+// console.log(admin);
+
+
+// typage fort ou faible
+
+// let distance = '12km';
+// distance = 12;
+// console.log(distance)
+
+// let ou const ?
+
+// let username = 'Claire'
+// const useRole = 1
+// username = 'Emmanuel'
+// console.log('alrighty')
+
+// const userage;
+// const pi;
+// let navLinks;
+// const earthweight;
+// const currentYear;
+// const datepicker;
+// const policePhoneNumber;
+// let userWeight;
---
+++ lesson1/04.data-types/script.js
@@ -0,0 +1,46 @@
+'use strict'
+// Data type
+
+
+    // // chaîne de caractères
+    // let user = 'Sophie'
+
+    // // nombre Integer
+    // let myNum = 1;
+    // let myInteger = 1;
+    // console.log('myInteger', myInteger)
+    // // nombre Float
+    // let myFloat = 1.12;
+    // console.log('myFloat', myFloat)
+    // // boolean
+    // let isTrue = true;
+    // let isFalse = false
+    // console.log('isTrue', isTrue)
+    // // valeur non définie
+    // let myUndefined = undefined;
+    // console.log('myUndefined', myUndefined)
+    // // valeur non existante
+
+  // String to Number
+    // console.log(Number('1'))
+
+    // let myString = '1';
+    // console.log(parseFloat(myString))
+
+    // console.log(+myString)
+
+
+    // let myString = 'hello'
+    // console.log(parseFloat(myString))
+
+    let myString = '0'
+    console.log(parseFloat(myString))
+
+
+// Caractère to number
+
+console.log(Number(yourStringHere))
+console.log(parseInt(yourStringHere))
+console.log(+yourStringHere)
+//Caractère to Boolean
+    console.log(boolean(yourStringHere))
