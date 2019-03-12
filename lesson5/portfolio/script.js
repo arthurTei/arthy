48ab8202926c62ddcc1eb010ae8f151c05d9f517
arthurTei <46706285+arthurTei@users.noreply.github.com>
2019-03-12 17:44:18 +0100

    lesson6

---
+++ lesson5/portfolio/script.js
@@ -0,0 +1,12 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+
+$('.js-timeline').Timeline();
+
---
+++ lesson6/04.media-optimisation/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// 1. Selectionner le lien
+// 2. Attacher un écouter d'événements à ce lien
+// 3. Quand le lien est cliqué, afficher ( selectionner et changer la propriété css 'display') le menu déroulant
+// 4. Quand le lien est re-cliqué, cacher le menu déroulent
+
+
+// FOOTER
+// Créer une variable pour récupérer la date d'aujourd'hui : new date().getFullYear()
+// Sélectionner l'élément du footer avec l'id js-current-year
+// Insérer la valeur d'année en cours dans l'élément sélectionné
+
